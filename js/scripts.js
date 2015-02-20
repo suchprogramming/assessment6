6a3bec949a9b3cb33ca929869c9333b4ad2c6d37
Tyler <whitsett.tyler@gmail.com>
2015-02-20 12:37:35 -0800

    add working topping selector and some validations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,47 @@
 var YummyPizza = {
   diameter: 0,
-  area: function(diameter) {
-    var area = Math.floor(3.14159 * (Math.pow(2,(diameter / 2))));
-    return area;
-  },
+  slices: 0,
   topping: null,
+  slicePizza: function(diameter) {
+    if (isNaN(diameter) || diameter < 1) {
+      return false;
+    } else {
+      var area = Math.floor(3.14159 * (Math.pow((diameter / 2), 2)));
+      if (area <= 50) {
+        return 4;
+      } else if (area > 50 && area < 114) {
+        return 6
+      } else {
+        return 8
+      }
+    }
+  },
 }
+
+$(document).ready(function() {
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+    $(".error").hide();
+
+    var inputtedPizzaDiameter = parseInt($("input#pizza-size").val());
+    var inputtedPizzaTopping = $("#new-pizza input[type='radio']:checked").val();
+
+    $("input#pizza-size").val("");
+
+    var newPizza = Object.create(YummyPizza);
+    newPizza.diameter = inputtedPizzaDiameter;
+    newPizza.slices = newPizza.slicePizza(newPizza.diameter);
+    newPizza.topping = inputtedPizzaTopping;
+
+    if (newPizza.slices) {
+      $(".result").show();
+      $("#pizza-result").text("Your Pizza Will Have" + " "
+      + newPizza.topping + " " + "and "
+      + newPizza.slices + " slices!");
+    } else {
+      $(".error .text").text(" Invalid or Blank Dimension!");
+      $(".result").hide();
+      $(".error").show();
+    }
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,24 @@
 describe("YummyPizza", function() {
-  describe("area", function() {
-    it("returns pizza area based on diameter", function() {
-      expect(YummyPizza.area(8)).to.equal(50);
+  describe("slices", function() {
+
+    it("returns an error if non-numerical values are entered", function() {
+      expect(YummyPizza.slicePizza("a")).to.equal(false);
+    });
+
+    it("returns pizza slices based on diameter", function() {
+      expect(YummyPizza.slicePizza(8)).to.equal(4);
+    });
+
+    it("returns pizza slices based on diameter", function() {
+      expect(YummyPizza.slicePizza(3)).to.equal(4);
+    });
+
+    it("returns pizza slices based on diameter", function() {
+      expect(YummyPizza.slicePizza(12)).to.equal(6);
+    });
+
+    it("returns pizza slices based on diameter", function() {
+      expect(YummyPizza.slicePizza(16)).to.equal(8);
     });
   });
 });
